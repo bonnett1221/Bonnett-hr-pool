0f1f523c5a992dde184b50035cc304f544883163
bonnett1221 <bonnett1221@gmail.com>
2026-03-26 15:47:49 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.CONFIG = {
+  SUPABASE_URL: "https://mborwmafjsjhozlchpba.supabase.co",
+  SUPABASE_KEY: "sb_publishable_1ID09EjcW1jC7k9dfPedUA_x3Oyix-G",
+  ADMIN_PASSCODE: "MLB20261221"
+};
